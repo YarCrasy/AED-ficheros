675821ab88d8cffd422d484a8b3373fba2bab67b
Gorka Jesius Quesada Vega <gorkajesusquesadavega@alumno.ieselrincon.es>
2026-01-09 19:45:24 +0000

    Commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module aed.elrincon {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.opencsv;
+    requires com.google.gson;
+    requires java.xml.bind; // Si usas JAXB
 
     opens aed.elrincon to javafx.fxml;
+    opens aed.elrincon.controller to javafx.fxml;
+
     exports aed.elrincon;
 }
