0a70dd2810e12eafd79473f500db13250ee083ef
Adrianbien43 <adribienmp43@gmail.com>
2026-01-09 17:19:36 +0100

    clase Student

--- src/main/java/aed/elrincon/model/Student.java
+++ src/main/java/aed/elrincon/model/Student.java
@@ -2,4 +2,47 @@ package aed.elrincon.model;
 
 public class Student {
 
+    private String nombre;
+    private String apellido;
+    private String edad;
+    private String matricula;
+
+    public Student(String nombre, String apellido, String edad, String matricula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
 }
