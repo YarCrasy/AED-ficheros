40f23f787196c78592cdac40bc719d50fcb7769f
GorkaJesus <gorkajesusquesadavega@alumno.ieselrincon.es>
2026-01-14 16:54:11 +0000

    Cambios realizados csv

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,12 @@ module aed.elrincon {
     requires javafx.fxml;
     requires com.opencsv;
     requires com.google.gson;
-    requires java.xml.bind; // Si usas JAXB
 
     opens aed.elrincon to javafx.fxml;
     opens aed.elrincon.controller to javafx.fxml;
+    opens aed.elrincon.model to javafx.fxml; // Necesario para PropertyValueFactory
 
     exports aed.elrincon;
+    exports aed.elrincon.controller;
+    exports aed.elrincon.model; // Exporta el modelo para TableView
 }
