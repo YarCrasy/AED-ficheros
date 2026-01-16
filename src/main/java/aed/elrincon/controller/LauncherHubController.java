3ffa072c763d7031f8c0be4858bebab9c9eae712
YarCrasy <contact@yarcrasy.com>
2026-01-16 11:45:58 +0000

    FIxedJSONPadding

--- src/main/java/aed/elrincon/controller/LauncherHubController.java
+++ src/main/java/aed/elrincon/controller/LauncherHubController.java
@@ -9,7 +9,23 @@ import javafx.fxml.FXML;
 public class LauncherHubController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
+    public void toCSV() throws IOException {
         App.setRoot("csv-view");
     }
+
+    @FXML
+    public void toJSON() throws IOException {
+        App.setRoot("json-view");
+    }
+
+    @FXML
+    public void toXML() throws IOException {
+        App.setRoot("xml-view");
+    }
+
+    @FXML
+    public void toOBJ() throws IOException {
+        App.setRoot("obj-view");
+    }
+
 }
